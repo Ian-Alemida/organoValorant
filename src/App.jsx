ccb8ff2da17ddc6faa4bb88c973d14a334c435ff
Ian Almeida <iangtaonline1@gmail.com>
2023-06-16 16:35:00 -0300

    clear workspace

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Banner from './Componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Banner/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          teste <code>src/App.js</code> teste dnv.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
