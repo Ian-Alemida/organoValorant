46a05c93286b0d8ff2ee749846181cfea73a9409
Ian Almeida <iangtaonline1@gmail.com>
2023-06-18 02:03:12 -0300

    mudando para projeto de familiarização com react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Banner from './Componentes/Banner/Banner';
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-        <Banner/>
-    </div>
+    <>
+      <h1>Mudando para projeto de familiarrização com react</h1>
+      <p className="texto">Os 3 movimentos: Expansão, Retração e interação</p>
+      <p className="texto">Os 4 elementos: Ar, Fogo, Agua, Terra</p>
+      <span className="texto">A forma de posicionar containers no React funciona exatamente igual a posicionar widgets no Flutter</span>
+    </>
   );
 }
 
