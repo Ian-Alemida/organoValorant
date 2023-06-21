ac9ee62c53b9f4a3b988a0f20bce9306e7caa8ca
Ian Almeida <iangtaonline1@gmail.com>
2023-06-21 00:16:20 -0300

    Ajustando aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./app.css";
 
 function App() {
+  const name = "Ian Almeida";
   return (
-    <>
-      <h1>Mudando para projeto de familiarrização com react</h1>
-      <p className="texto">Os 3 movimentos: Expansão, Retração e interação</p>
-      <p className="texto">Os 4 elementos: Ar, Fogo, Agua, Terra</p>
-      <span className="texto">A forma de posicionar containers no React funciona exatamente igual a posicionar widgets no Flutter</span>
-    </>
+    <div>
+      <h1 id="tit">Ola {name}</h1>
+      <button onClick={console.log("apertou")}>My button</button>.
+    </div>
   );
 }
 
