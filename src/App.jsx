c3cef434ef746c70af9f9f50cf3623327bcbf9c0
Ian Almeida <iangtaonline1@gmail.com>
2023-06-28 00:32:33 -0300

    configurando header

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
       <Header/>
-      <Main/>
+      <Main cor={"red"} nome="blog de ian 3" conteudo={"Conteudo de teste para ver como as props se comportam"}/>
+      <Main cor={"#44ff88"} nome={"ihuul"} conteudo={"Elas se comportam igual no flutter, la tbm tem uma função onde é possivel definir propriedades para widgets que serão criados. a propriedade é usada em lugares que serão modificados, sendo assim possuem um valor variavel. Para usar basta declarar o nome da variavel na abertura da função que criará o container, e posiciona-la em  seu devido lugar dentro do codigo, depois elas podem serr chamadas e atribuidas o valor que desejar na chamada do conntainer."}></Main>
       <Footer/>
     </>
   );
--- src/Componentes/Header/header.jsx
+++ src/Componentes/Header/header.jsx
@@ -1,7 +1,11 @@
-function Header (){
+import "./header.css"
+import  banner from "./Zenitsu.webp"
+
+function Header ({titulo}){
     return (
         <header>
-            <h1>Meu blog!</h1>
+            <h1>Blog Alquimico</h1>
+            <img src={banner} alt="banner da pagina"/>
         </header>
     )
 }
