d8cc61bb76f9ecfdd7d8a87696a70d678d42db6f
Ian Almeida <iangtaonline1@gmail.com>
2023-09-27 20:45:58 -0300

    create colaborador component

---
+++ src/componentes/Colaborador/Colaborador.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src="https://github.com/ian-alemida.png" alt="imagem do colaborador" />
+            </div>
+            <div>
+                <h4>Ian Almeida</h4>
+                <h5>Estudante</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Colaborador;
