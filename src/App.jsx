ac9c23333229b36a8481f0ecacfdc50bd4f3bbc7
Ian Almeida <iangtaonline1@gmail.com>
2023-09-30 17:48:56 -0300

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
   const times = [
