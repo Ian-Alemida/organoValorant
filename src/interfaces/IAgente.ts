db549e4883e9a9028fe254912ed04cb7a1256969
ian-alemida <iangtaonline1@gmail.com>
2024-10-04 12:39:53 -0300

    baixando pacote do uuid para o  typescript e ajustando IAgente

--- src/interfaces/IAgente.ts
+++ src/interfaces/IAgente.ts
@@ -1,10 +1,10 @@
 export default interface IAgente {
     cor?: string,
-    classe: string,
+    classe?: string,
     favorito?: boolean,
     nome: string,
-    imagem: string,
-    funcao: string,
+    imagem?: string,
+    funcao?: string,
     id?: string,
     key?: string | React.Key | null | undefined
 }
